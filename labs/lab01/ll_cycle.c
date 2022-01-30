fc8ff7b22b52a20503507beff97f216eb3128c41
GrassCube <3180101353@zju.edu.cn>
2022-01-30 17:29:12 +0800

    finish lab1-2

--- labs/lab01/ll_cycle.c
+++ labs/lab01/ll_cycle.c
@@ -1,7 +1,19 @@
-#include <stddef.h>
 #include "ll_cycle.h"
+#include <stddef.h>
 
 int ll_has_cycle(node *head) {
-    /* your code here */
-    return 0;
+  node *tortoise, *hare;
+  tortoise = hare = head;
+  while (1) {
+    if (tortoise &&  tortoise->next)
+      tortoise = tortoise->next;
+    else
+      return 0;
+    if (hare && hare->next && hare->next->next)
+      hare = hare->next->next;
+    else
+      return 0;
+    if (tortoise == hare)
+      return 1;
+  }
 }
---
+++ labs/notes.md
@@ -0,0 +1,13 @@
+# lab1
+
+## gdb basics
+|functions | commands|
+|:--:|:--:|
+|run with arguments|`r <args>`|
+|create breakpoints|`b <functionName>`|
+|next line|`n`|
+|step into|`s`|
+|continue|`c`|
+|print var|`p <var>`|
+|display var every time program stops|`display <var>`|
+|show all local variables|`info locals`|
